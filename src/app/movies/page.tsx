bc728c9720bcbb9fc286302645e7a51754ee55e2
gravender <ss.noah@gmail.com>
2024-01-12 12:28:25 +1100

    feat: :sparkles: improve multi xcreensixe support

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -50,20 +50,20 @@ type movieCarouselProps = {
 const MovieCarousel = ({ movies, title }: movieCarouselProps) => {
   const router = useRouter();
   return (
-    <div className="flex w-full flex-col items-center justify-center">
+    <div className="flex w-full max-w-7xl flex-col items-center justify-center">
       <CardTitle className="w-full pl-12 text-left">{`${title}:`}</CardTitle>
       <Carousel
         opts={{
           align: "start",
           loop: true,
         }}
-        className="mt-4 w-full lg:max-w-5xl 2xl:max-w-7xl"
+        className="mt-4 w-full max-w-xs sm:max-w-xl md:max-w-2xl lg:max-w-4xl xl:max-w-6xl"
       >
         <CarouselContent>
           {movies.map((movie) => (
             <CarouselItem
               key={title + movie.id}
-              className="md:basis-1/3 lg:basis-1/4 xl:basis-1/5 2xl:basis-1/6"
+              className="sm:basis-1/2 md:basis-1/3 lg:basis-1/4 xl:basis-1/5 2xl:basis-1/6"
             >
               <Card
                 className="h-96 w-full"
